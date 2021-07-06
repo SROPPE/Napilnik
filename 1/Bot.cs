100b24716ddf7992d81831744ad8eeb1d30f0a29
a.kazimir <a.a199@bk.ru>
2021-07-07 00:02:08 +0300

    Refactoring

--- 1/Bot.cs
+++ 1/Bot.cs
@@ -1,14 +1,19 @@
-﻿public class Bot
+﻿using System;
+
+public class Bot
 {
     private readonly Weapon _weapon;
 
     public Bot(Weapon weapon)
     {
-        _weapon = weapon;
+        _weapon = weapon ?? throw new ArgumentNullException(nameof(weapon));
     }
 
     public void OnSeePlayer(Player player)
     {
+        if (player == null)
+            throw new ArgumentNullException(nameof(player));
+
         _weapon.TryFire(player);
     }
 }
--- 1/Player.cs
+++ 1/Player.cs
@@ -1,10 +1,12 @@
-﻿public class Player : IDamageable
+﻿using System;
+
+public class Player : IDamageable
 {
     private Health _health;
 
     public Player(Health health)
     {
-        _health = health;
+        _health = health ?? throw new ArgumentNullException(nameof(health));
     }
 
     public void TakeDamage(Damage damage)
